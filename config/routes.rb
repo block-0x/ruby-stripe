fad47fe39977f1a09d9608b7fc5a3f7f5cddd365
Iida-Jun <jun_iida@spice-factory.co.jp>
2021-08-10 14:51:20 +0900

    create

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :posts
+  resources :users, only: [:show, :index]
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+end
